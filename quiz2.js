25825d4ce8545e38cc0783df661ce999c347d323
jessicapierre23 <jessica.pierre23@gmail.com>
2014-04-25 15:54:52 -0400

    commit

--- quiz2.js
+++ quiz2.js
@@ -33,7 +33,7 @@ getAnswer();
 }
 
   function startOver() {
-			      //questionNum = 0;//THIS DOESN'T SEEM TO WORK HERE; HAVE TO PUT THIS LINE BF STARTOVER 
+			      
 				  return showQuestion(0);
 				  console.log('questionNum',questionNum);
 			
@@ -59,20 +59,18 @@ function getAnswer() {
 		$('li').remove();
 		$('input[type=submit]').remove();
 		$('#question-holder').empty()//this keeps the questions from shifting down
-		//$('#sm').removeClass('hidden');
-		//$("#question-holder").append($('#sm')).fadeIn('fast');
-		//$('#sm').addClass('hidden');
+		//sm
 		questionNum = 0;
 		$("#question-holder").append('<h5>' + explanations[0].title + '</h5><img class="grr" src="nosmoke.png" /><p>' + explanations[0].para + '</p>').fadeIn('fast');
 		$("#question-holder").append("<br/><input type='submit' value='Start Over' onclick = 'showQuestion(0);' class='fancy'/>").fadeIn('fast');
-		//console.log('questionNum',questionNum);
+		
 		
 		}
 		else {
 		
 		questionNum = 1;
 		showQuestion(1);
-		//console.log('questionNum',questionNum);
+		
 		}
 		break;
    
@@ -85,7 +83,7 @@ function getAnswer() {
 		$('#question-holder').empty();//this keeps the questions from shifting down
 		questionNum = 2;
 		showQuestion(2);
-		//console.log('questionNum',questionNum);
+		
 		}
 		else {
 		$('h5').remove();
@@ -94,7 +92,7 @@ function getAnswer() {
 		$('#question-holder').empty();//this keeps the questions from shifting down
 		questionNum = 3;
 		showQuestion(3);
-		//console.log('questionNum',questionNum);
+		
 		}
 		break;
 		
@@ -105,13 +103,12 @@ function getAnswer() {
 		$('li').remove();
 		$('input[type=submit]').remove();
 		$('#question-holder').empty();//this keeps the questions from shifting down
-		//$('#ar').removeClass('hidden');
-		//$("#question-holder").append($('#ar')).fadeIn('fast');
+		//ar
 		questionNum = 0;
 		$("#question-holder").append('<h5>' + explanations[1].title + '</h5><img class="grr" src="antacid.png" /><p>' + explanations[1].para + '</p>').fadeIn('fast');
 		
 		$("#question-holder").append("<br/><input type='submit' value='Start Over' onclick = 'showQuestion(0);' class='fancy'/>").fadeIn('fast');
-		//console.log('questionNum',questionNum);
+	
 		}
 		else {
 		$('h5').remove();
@@ -120,7 +117,7 @@ function getAnswer() {
 		$('#question-holder').empty();//this keeps the questions from shifting down
 		questionNum = 4;
 		showQuestion(4);
-		//console.log('questionNum',questionNum);
+		
 		}
 		break;
 		
@@ -132,21 +129,19 @@ function getAnswer() {
 		$('#question-holder').empty();//this keeps the questions from shifting down
 		
 		if (questions[questionNum].options[selection] ==  questions[questionNum].options[0]) {
-		//$('#pn').removeClass('hidden');
-		//$("#question-holder").append($('#pn')).fadeIn('fast');
+		//pn
 		questionNum = 0;
 		$("#question-holder").append('<h5>' + explanations[2].title + '</h5><img class="grr" src="med.png" /><p>' + explanations[2].para + '</p>').fadeIn('fast');
 		$("#question-holder").append("<br/><input type='submit' value='Start Over' onclick = 'showQuestion(0);' class='fancy'/>").fadeIn('fast');
-		//console.log('questionNum',questionNum);
+		
 		}
 		
 		else {
-		//$('#br').removeClass('hidden');
-		//$("#question-holder").append($('#br')).fadeIn('fast');
+		//br
 		questionNum = 0;
 		$("#question-holder").append('<h5>' + explanations[3].title + '</h5><img class="grr" src="lungs.png" /><p>' + explanations[3].para + '</p>').fadeIn('fast');
 		$("#question-holder").append("<br/><input type='submit' value='Start Over' onclick = 'showQuestion(0);' class='fancy'/>").fadeIn('fast');
-		//console.log('questionNum',questionNum);
+		
 		}
 		
 		break;
@@ -157,12 +152,11 @@ function getAnswer() {
 		$('li').remove();
 		$('input[type=submit]').remove();
 		$('#question-holder').empty();//this keeps the questions from shifting down
-		//$('#pnd').removeClass('hidden');
-		//$("#question-holder").append($('#pnd')).fadeIn('fast');
+		//pnd
 		questionNum = 0;
 		$("#question-holder").append('<h5>' + explanations[4].title + '</h5><img class="grr" src="water.png" /><p>' + explanations[4].para + '</p>').fadeIn('fast');
 		$("#question-holder").append("<br/><input type='submit' value='Start Over' onclick = 'showQuestion(0);' class='fancy'/>").fadeIn('fast');
-		//console.log('questionNum',questionNum);
+		
 		}
 		else {
 		$('h5').remove();
@@ -171,7 +165,7 @@ function getAnswer() {
 		$('#question-holder').empty();//this keeps the questions from shifting down
 		questionNum = 5;
 		showQuestion(5);
-		//console.log('questionNum',questionNum);
+		
 		}
 		break;
 		case 5:
@@ -181,12 +175,11 @@ function getAnswer() {
 		$('li').remove();
 		$('input[type=submit]').remove();
 		$('#question-holder').empty();//this keeps the questions from shifting down
-		//$('#aa').removeClass('hidden');
-		//$("#question-holder").append($('#aa')).fadeIn('fast');
+		//aa
 		questionNum = 0;
 		$("#question-holder").append('<h5>' + explanations[5].title + '</h5><img class="grr" src="inhaler.png" /><p>' + explanations[5].para + '</p>').fadeIn('fast');
 		$("#question-holder").append("<br/><input type='submit' value='Start Over' onclick = 'showQuestion(0);' class='fancy'/>").fadeIn('fast');
-		//console.log('questionNum',questionNum);
+		
 		}
 		else {
 		$('h5').remove();
@@ -195,7 +188,7 @@ function getAnswer() {
 		$('#question-holder').empty();//this keeps the questions from shifting down
 		questionNum = 6;
 		showQuestion(6);
-		//console.log('questionNum',questionNum);
+		
 		}
 		break;
 		case 6:
@@ -205,20 +198,18 @@ function getAnswer() {
 		$('#question-holder').empty();//this keeps the questions from shifting down
 		
 		if (questions[questionNum].options[selection] ==  questions[questionNum].options[0]) {
-		//$('#se').removeClass('hidden');
-		//$("#question-holder").append($('#se')).fadeIn('fast');
+		//se
 		questionNum = 0;
 		$("#question-holder").append('<h5>' + explanations[6].title + '</h5><img class="grr" src="pills2.png" /><p>' + explanations[6].para + '</p>').fadeIn('fast');
 		$("#question-holder").append("<br/><input type='submit' value='Start Over' onclick = 'showQuestion(0);' class='fancy'/>").fadeIn('fast');
-		//console.log('questionNum',questionNum);
+		
 		}
 		else {
-		//$('#cf').removeClass('hidden');
-		//$("#question-holder").append($('#cf')).fadeIn('fast');
+		//cf
 		questionNum = 0;
 		$("#question-holder").append('<h5>' + explanations[7].title + '</h5><img class="grr" src="sick.png" /><p>' + explanations[7].para + '</p>').fadeIn('fast');
 		$("#question-holder").append("<br/><input type='submit' value='Start Over' onclick = 'showQuestion(0);' class='fancy'/>").fadeIn('fast');
-		//console.log('questionNum',questionNum);
+		
 		}
 		
 		break;
